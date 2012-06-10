a370c3d96daf43124167affb1c150fed7c6e55c4
dhbikoff <dhbikoff@gmail.com>
2012-06-10 11:09:46 -0400

    removed test

--- vector-hashset/test.c
+++ vector-hashset/test.c
@@ -1,21 +1,53 @@
 #include "vector.h"
 #include <stdio.h>
 #include <string.h>
+#include <ctype.h>
+
+int cmp(const void *a, const void*b)
+{
+  int one;
+  memcpy(&one,(int*)a,sizeof(int));
+  printf("ONE = %d\n", one);
+  int two;
+  memcpy(&two,(int*)b,sizeof(int));
+  printf("TWO = %d\n", two);
+
+  if (one > two) return 1;
+  if (two > one) return -1;
+  return 0;
+}
+
+void mapFn(void *elemAddr, void* auxData)
+{
+  printf("elem = %d\n", *(int*)elemAddr);
+}
 
 int main()
 {
   vector v;
-  VectorNew(&v, sizeof(int), NULL, 4);
-  for (int i = 10; i >= 0; i--) {
+  VectorNew(&v, sizeof(char), NULL, 4);
+  int num = 0;
+  for (char i = 'A'; i <= 'Z'; i++) {
+    printf("%c",i);
     VectorAppend(&v,&i);
+    printf("%c , %d\n",*(char*)VectorNth(&v,num), num);
+    num++;
   }
 
+  /*
   for (int i = 0; i < VectorLength(&v); i++) {
-    int *p = VectorNth(&v, i);
-    printf("%d\n", *p);
+    char *elem = (char*) VectorNth(&v,i);
+    *elem = tolower(*elem);
   }
-  VectorDispose(&v);
+  */
+  printf("length = %d\n", VectorLength(&v));
+  for (int i = 0; i < VectorLength(&v); i++) {
+    char *elem = (char*)VectorNth(&v,i);
+    printf("%d  ", i);
+    printf("\n%c\n", *elem);
+  }
 
+  VectorDispose(&v);
   return 0;
 }
 
